210779d9e1d7257d18f658eebf877dab35c99553
abhay2132 <abhaybro768@gmail.com>
2023-04-23 13:02:02 +0000

    yaml env setup

--- bin/pages.config.js
+++ bin/pages.config.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 const e = (view, title) => ({view, title})
 const {resolve: r , join: j} = require("path")
 const pages = {
@@ -5,10 +7,32 @@ const pages = {
     "/about":e('about', "About")
 }
 const viewDir = j(r(), "views")
+var base = "/"
+if(fs.existsSync(j(r(),".env"))){
+    let env = parseEnv(j(r(),".env"))
+    // console.log(env)
+    base = env.get("BASE_URL") || base;
+}
+
 module.exports = {
     viewDir,
-    base : process.env.BASE_URL || "/",
+    base,
     ext : "ejs",
     baseLayout: j(viewDir, "layouts", "main.ejs"),
     pages
+}
+
+// return env map
+function parseEnv (env){ 
+    let map = new Map();
+    let data = fs.readFileSync(env).toString();
+    data.split("\n")
+    .forEach(line=>{
+        let parts = line.split("=")
+        if(parts.length>2) parts[1] = parts.slice(1).join("");
+        let name = parts[0].trim();
+        let value = eval(parts[1].trim())
+        map.set(name, value)
+    })
+    return map;
 }
