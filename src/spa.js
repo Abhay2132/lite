3f44c518d94e210176ce45ed0ce8adcb94d30137
abhay2132 <abhaybro768@gmail.com>
2023-04-30 14:20:36 +0000

    fixed router

--- src/spa.js
+++ src/spa.js
@@ -1,10 +1,10 @@
 import { log, $, $$ } from "./hlpr.js"
 
+const ef = () => { }
 const base = window.base || ""
-const href2url = (href) => "/"+[base, "_spa", href+"_.json"].map(i => i.replace(/[\/]/g,'')).filter(Boolean).join("/");
-const dp = new DOMParser();
+const href2url = (href) => "/" + [base, "_spa", href + "_.json"].map(i => i.replace(/[\/]/g, '')).filter(Boolean).join("/");
 const pages = new Map();
-const href2route = href => href.startsWith(base) ? href : base+href;
+const normalizeURL = (url)=> (url[0] == '/' && '/')+url.split("/").filter(Boolean).join('/')
 
 export const _history = {
 	e: new Map(),
@@ -12,25 +12,27 @@ export const _history = {
 	pos: 0,
 	replace(url = "", state = {}) {
 		// this.states[Math.max(this.states.length, 1) - 1] = { url, state };
-		this.states[this.pos] = {url, state}
-		history.replaceState({pos: this.pos}, null, url);
+		this.states[this.pos] = { url, state }
+		history.replaceState({ pos: this.pos }, null, url);
 		this.emit('replace')
 	},
 	push(url = "", state = {}) {
-		if(this.pos < this.states.length - 1) this.states = this.states.slice(0, pos);
+		if (this.pos < this.states.length - 1) this.states = this.states.slice(0, pos);
 		// this.states.push({ url, state });
 		this.pos += 1;
-		this.states[this.pos] = {url, state}
-		history.pushState({pos: this.pos}, null, url)
+		this.states[this.pos] = { url, state }
+		history.pushState({ pos: this.pos }, null, url)
 		this.emit('push')
 	},
-	on(name = false, handler = false) {
-		if (!(name && handler)) throw new Error(`Either name or Handler is missing in \`on\` method`)
-		if (!this.e.has(name)) this.e.set(name, new Map())
-		let e = this.e.get(name)
+	on(names = false, handler = false) {
+		if (!(names && handler)) throw new Error(`Either name or Handler is missing in \`on\` method`)
+		names.split(" ").forEach(name => {
+			if (!this.e.has(name)) this.e.set(name, new Map())
+			let e = this.e.get(name)
 
-		for (let [key, val] of e) if (Object.is(val.handler, handler)) return;
-		e.set(e.size, { cleanup: false, handler })
+			for (let [key, val] of e) if (Object.is(val.handler, handler)) return;
+			e.set(e.size, { cleanup: false, handler })
+		})
 	},
 	emit(name, data) {
 		// log('emit', name)
@@ -57,69 +59,72 @@ export const _history = {
 
 window.addEventListener('popstate', (e) => _history.emit('pop', e));
 
-async function navigate(o, reInit) {
-	const { href, outlet, onerror, onstart, onappend, pop = false } = o;
+async function Navigate(o) {
+	const { href, outlet, onerror, onstart, onappend, pop = false, reInit = ef } = o;
 	onstart();
-
 	let url = href2url(href);
-	//log({url})
 	if (!pages.has(url)) pages.set(url, await (await fetch(url)).json())
 	var data = pages.get(url);
-
 	let { error = false, html = "", css = "", js = "" } = data;
 	if (error) onerror(error, void console.error(error));
-
 	outlet.innerHTML = html;
-	if (!pop) {
-		_history.replace('', { href: location.pathname.slice(base.length) })
-		_history.push(base + href, { href });
-	}
 	o.onappend(error)
-	reInit(); // re attach the navigator to the new links in outlet
+	reInit();
 }
 
-/* ---------------------------------------------------------------------------------------- */
-function attachNavigator(a, o, cb) {
+/* -------------------------------Extract Routes for <a> tag------------------------------------------ */
+function ExtractRoutes(a) {
 	if (a.getAttribute('spa-attached')) return;
-
 	a.setAttribute('spa-attached', true);
+
+	while (1) {
+		if (a.tagName.toLowerCase() == "a") break;
+		a = a.parentNode;
+	}
+	let href = a.getAttribute("href")
+	Router.routes.add(normalizeURL(base + href))
+
 	a.addEventListener("click", async e => {
-
 		e.preventDefault();
-		let a = e.target;
-		while (1) {
-			if (a.tagName.toLowerCase() == "a") break;
-			a = a.parentNode;
-		}
-		let href = a.getAttribute("href")
-
-		// log('before navigate',e)
-		await navigate({ ...o, href }, cb)
+		_history.push(base + href);
 
 	}, false)
 }
 
-const ef = () => { } // empty function 
-var inited = false;
+const Router = {
+	attach: ef,
+	routes: new Set(),
+	navigate() {
+		let pathname = normalizeURL(location.pathname)
+		if (!this.routes.has(pathname)) return console.log(Router.routes, location.pathname)
+		Navigate({ ...this.o, href: pathname.slice(base.length), reInit: ()=>init(this.o)})
+	},
+	o:{}
+}
+
+_history.on("push pop", (...a) => Router.navigate(...a))
+
+/* `init` makes the initial setups like extracting routes from <a> tags 
+and setup `Router`
+
+@return <void>
+@params {
+	outlet <HTMLElement>,
+	onstart, onappend, onerror <function> 
+} */
 export function init(o) {
-	const { outlet = false, onstart = ef, onappend = ef, onerror = console.error } = o
+	const { outlet = false } = o;
 
 	if (!outlet) throw new Error(`outlet is not defined`)
-	$$('[spa-link]').forEach(a => attachNavigator(a, o, ()=> init(o)));
+	$$('[spa-link]').forEach(ExtractRoutes);
 
-	if (inited) return;
-
-	_history.on('pop', function (me,e) {
-		// me.states.pop();
-		//console.log(e.type, history)
-		let {state:{pos}} = history;
-		if(typeof pos != 'number') return console.log(history.state);
-		if(_history.states.length ==	0) return;
-		
-		let { state: { href }} = _history.states[pos];
-		// console.log('_history pop event fired ', me.state)
-		navigate({ ...o, href, pop: true }, ()=>init(o));
-	})
-
-	inited = true;
+	// The Following code gonna run once only
+	if (init.inited) return;
+	Router.o = o
+	init.inited = true;
 }
+
+(async ()=>{
+	let url = location.pathname;
+	if (!pages.has(url)) pages.set(url, await (await fetch(url)).json())
+})();
