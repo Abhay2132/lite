e81c7cccd58a92d37fe6a39310cd4c9cf5f68d7e
abhay2132 <abhaybro768@gmail.com>
2023-05-18 17:42:38 +0000

    added base in sw

--- app/src/sw.js
+++ app/src/sw.js
@@ -1,8 +1,8 @@
 export  async function registerServiceWorker () {
   if ("serviceWorker" in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "/",
+      const registration = await navigator.serviceWorker.register(base+"/sw.js", {
+        scope: base+"/",
       });
     } catch (error) {
       console.error(`Registration failed with ${error}`);
